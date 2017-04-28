f5a5d70b054e8b89fe1131e1bf5c62cbbde4ee5a
Clint Avalos <clint.avalos@padi.com>
2017-04-28 16:25:22 -0700

    Changed D8 install to use drush instead of console. - Drupal console now only supports being installed per project with composer.

--- barista/apps/drupal8/install.sh
+++ barista/apps/drupal8/install.sh
@@ -18,36 +18,22 @@ then
   install_drush
 fi
 
-# Check/Install Drupal Console.
-cmd_exists drupal
-if [ ! "$?" = 1 ];
-then
-  install_drupal_console
-fi
-
-drupalroot=$docroot"/web"
-
 # Install Drupal 8.
 echo_style 2 "Installing Drupal 8."
-cd $docroot && drupal init -n
-cd $docroot && drupal site:new --placeholder="directory:"$docroot -n
-cd $docroot && drush sql-drop --db-url='mysql://'$db_user':'$db_pw'@'$db_host'/'$db_name -y
-cd $docroot && drupal site:install standard \
-  --langcode="en" --db-type="mysql" --db-host=$db_host \
-  --db-name=$db_name --db-user=$db_user --db-pass=$db_pw \
-  --db-port=$db_port --site-name="Expresso PHP - Drupal 8" --site-mail="admin@example.com" \
-  --account-name="admin" --account-mail="admin@example.com" --account-pass="password" \
-  --force --no-interaction
+cd $projroot && drush dl drupal-8 --drupal-project-rename=web -y
+cd $webroot && drush sql-drop --db-url='mysql://'$db_user':'$db_pw'@'$db_host'/'$db_name -y
+cd $webroot && drush si standard \
+  --db-url='mysql://'$db_user':'$db_pw'@'$db_host'/'$db_name -y \
+  --site-name=ExpressoPHP --locale=en \
+  --account-pass=admin --account-mail=admin@example.com \
 
-# Configuring .htaccess.
-echo_style 2 "Configuring .htaccess."
-cp $baristaroot/apps/$env/.htaccess.docroot $docroot/.htaccess
-sed -i 's|# RewriteBase /drupal|RewriteBase /web|g' $docroot/web/.htaccess
+# Install Drupal Console.
+cd $webroot && composer require drupal/console:~1.0
 
 # Clear cache.
 echo_style 2 "Claering cache..."
-cd $drupalroot && drupal cache:rebuild all
+cd $webroot && drush cr all
 
 # Create ULI.
 echo_style 2 "Your Drupal ULI is: "
-cd $drupalroot && drush uli
+cd $webroot && drush uli
